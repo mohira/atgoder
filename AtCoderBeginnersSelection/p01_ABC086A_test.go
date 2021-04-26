e42e5d75ab26b9c8ef5e0080fa7989b67b7a522e
mohira <shukatsudaison@gmail.com>
2021-04-26 20:28:52 +0900

    AtCoderBeginnersSelection: ABC086A

---
+++ AtCoderBeginnersSelection/p01_ABC086A_test.go
@@ -0,0 +1,37 @@
+package AtCoderBeginnersSelection
+
+import "testing"
+
+func AnswerABC086A(a int, b int) string {
+	product := a * b
+
+	if product%2 == 0 {
+		return "Even"
+	} else {
+		return "Odd"
+	}
+}
+
+func TestAnswerABC086A(t *testing.T) {
+	tests := []struct {
+		name string
+		a    int
+		b    int
+		want string
+	}{
+		{"入力例1", 3, 4, "Even"},
+		{"入力例2", 1, 21, "Odd"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := AnswerABC086A(tt.a, tt.b)
+
+			if got != tt.want {
+				t.Errorf("got %v want %v", got, tt.want)
+			}
+		})
+
+	}
+
+}
