c8e1b95da8230c6e39dd797839f8cf0f199ecfd6
mohira <shukatsudaison@gmail.com>
2021-04-27 12:14:49 +0900

    AtCoderBeginnersSelection: 類題: ABC157A

---
+++ AtCoderBeginnersSelection/p01_ABC086A/similar/ABC157A_test.go
@@ -0,0 +1,32 @@
+package similar
+
+import (
+	"testing"
+)
+
+// [ABC157A - Duplex Printing](https://atcoder.jp/contests/abc157/tasks/abc157_a)
+func AnswerABC157A(N int) int {
+	return N/2 + N%2
+}
+
+func TestAnswerABC157A(t *testing.T) {
+	tests := []struct {
+		name string
+		N    int
+		want int
+	}{
+		{"入力例1", 5, 3},
+		{"入力例2", 2, 1},
+		{"入力例3", 100, 50},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := AnswerABC157A(tt.N)
+
+			if got != tt.want {
+				t.Errorf("got %v want %v", got, tt.want)
+			}
+		})
+	}
+}
