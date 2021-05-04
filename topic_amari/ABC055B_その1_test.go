8db6c69820b64dc84f3cf6435b04503dd990fb67
mohira <shukatsudaison@gmail.com>
2021-05-04 17:04:04 +0900

    余りの問題: ABC055B 積の余りの性質

---
+++ topic_amari/ABC055B_その1_test.go
@@ -0,0 +1,42 @@
+package p08_ABC085C
+
+import (
+	"testing"
+)
+
+// [ABC055B - Training Camp](https://atcoder.jp/contests/abc055/tasks/abc055_b)
+func AnswerABC055Bその1(N int) int {
+	// 積abの余りは、a,bそれぞれの余りの積の余り
+	// (ab mod X) = (a mod X) (b mod X) mod X
+	// つまり、毎回、余りの計算をして良い
+	const X = 1000000007
+	var power = 1
+
+	for i := 1; i <= N; i++ {
+		power = (power * i) % X
+	}
+
+	return power
+}
+
+func TestAnswerABC055Bその1(t *testing.T) {
+	tests := []struct {
+		name string
+		N    int
+		want int
+	}{
+		{"入力例1", 3, 6},
+		{"入力例2", 10, 3628800},
+		{"入力例3", 100000, 457992974},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := AnswerABC055Bその1(tt.N)
+
+			if got != tt.want {
+				t.Errorf("got %v want %v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ topic_amari/README.md
@@ -0,0 +1,7 @@
+# 余りの性質
+
+## [ABC055B - Training Camp](https://atcoder.jp/contests/abc055/tasks/abc055_b)
+
+<img width="1876" alt="image.png (3.2 MB)" src="https://img.esa.io/uploads/production/attachments/6586/2021/05/04/21054/ee2ec75d-fa7c-4510-bfdf-7a6380916d96.png">
+
+
