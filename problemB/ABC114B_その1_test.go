85c6b97e36f60f947fba63acc53962419f35ff0a
mohira <shukatsudaison@gmail.com>
2021-05-25 13:59:01 +0900

    ABC114B 素直に実装

---
+++ problemB/ABC114B_その1_test.go
@@ -0,0 +1,46 @@
+package problemB
+
+import (
+	"math"
+	"strconv"
+	"testing"
+)
+
+// [ABC114B - Christmas Eve Eve](https://atcoder.jp/contests/abc114/tasks/abc114_b)
+func AnswerABC114Bその1(S string) int {
+	var minDiff = math.MaxInt64
+	for i := 0; i <= len(S)-3; i++ {
+		X, err := strconv.Atoi(S[i : i+3])
+
+		if err != nil {
+			panic(err)
+		}
+
+		absDiff := absInt(X - 753)
+		minDiff = min(minDiff, absDiff)
+	}
+
+	return minDiff
+}
+
+func TestAnswerABC114Bその1(t *testing.T) {
+	tests := []struct {
+		name string
+		S    string
+		want int
+	}{
+		{"入力例1", "1234567876", 34},
+		{"入力例2", "35753", 0},
+		{"入力例3", "1111111111", 642},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := AnswerABC114Bその1(tt.S)
+
+			if got != tt.want {
+				t.Errorf("got %v want %v", got, tt.want)
+			}
+		})
+	}
+}
