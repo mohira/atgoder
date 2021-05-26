9a1dd48b689070deefc1f626c759546bc9c85520
mohira <shukatsudaison@gmail.com>
2021-05-26 21:45:00 +0900

    ABC096B 最大値を2倍しつづければOK

---
+++ problemB/ABC096B_その1_test.go
@@ -0,0 +1,40 @@
+package problemB
+
+import (
+	"testing"
+)
+
+// [ABC096B - Maximum Sum](https://atcoder.jp/contests/abc096/tasks/abc096_b)
+func AnswerABC096Bその1(A, B, C, K int) int {
+	nums := []int{A, B, C}
+	maxV := findMax(nums)
+	total := sumInts(nums)
+
+	double := maxV
+	for i := 0; i < K; i++ {
+		double *= 2
+	}
+	return total - maxV + double
+}
+
+func TestAnswerABC096Bその1(t *testing.T) {
+	tests := []struct {
+		name    string
+		A, B, C int
+		K       int
+		want    int
+	}{
+		{"入力例1", 5, 3, 11, 1, 30},
+		{"入力例2", 3, 3, 4, 2, 22},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := AnswerABC096Bその1(tt.A, tt.B, tt.C, tt.K)
+
+			if got != tt.want {
+				t.Errorf("got %v want %v", got, tt.want)
+			}
+		})
+	}
+}
