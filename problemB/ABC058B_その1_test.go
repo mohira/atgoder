ebe43ffc35866ea8bb0fb27baa620899f41a5cd5
mohira <shukatsudaison@gmail.com>
2021-05-27 22:44:39 +0900

    ABC059B そのままやればOK / strings.Builderを使ってみた / [【改訂版】文字列連結はどれが速い？ | text.Baldanders.info](https://text.baldanders.info/golang/join-strings-2/)

---
+++ problemB/ABC058B_その1_test.go
@@ -0,0 +1,48 @@
+package problemB
+
+import (
+	"strings"
+	"testing"
+)
+
+// [ABC058B - Comparison](https://atcoder.jp/contests/abc058/tasks/abc058_b)
+func AnswerABC058Bその1(O, E string) string {
+	lenO := len(O)
+	lenE := len(E)
+
+	var out strings.Builder
+	out.Grow(lenO + lenE)
+
+	for i := 0; i < lenE; i++ {
+		out.WriteByte(O[i])
+		out.WriteByte(E[i])
+	}
+
+	if lenO-lenE == 1 {
+		out.WriteByte(O[lenO-1])
+	}
+
+	return out.String()
+}
+
+func TestAnswerABC058Bその1(t *testing.T) {
+	tests := []struct {
+		name string
+		O, E string
+		want string
+	}{
+		{"入力例1: 文字数が等しい", "xyz", "abc", "xaybzc"},
+		{"入力例2: 文字数が等しくない", "atcoderbeginnercontest", "atcoderregularcontest", "aattccooddeerrbreeggiunlnaerrccoonntteesstt"},
+		{"", "ac", "b", "abc"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := AnswerABC058Bその1(tt.O, tt.E)
+
+			if got != tt.want {
+				t.Errorf("got %v want %v", got, tt.want)
+			}
+		})
+	}
+}
