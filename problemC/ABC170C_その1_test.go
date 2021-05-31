0b4d044f158194d0a190320c156c9ab779d8fde8
mohira <shukatsudaison@gmail.com>
2021-05-31 18:45:55 +0900

    ABC170C 冷静に解の範囲を考える / in演算子とか欲しい / 条件をつけてもいいけど、全探索もスッキリかける

---
+++ problemC/ABC170C_その1_test.go
@@ -0,0 +1,74 @@
+package problemC
+
+import (
+	"atgoder/lib"
+	"testing"
+)
+
+// [ABC170C - Forbidden List](https://atcoder.jp/contests/abc170/tasks/abc170_c)
+func AnswerABC170Cその1(X, N int, P []int) int {
+	// 解は[0, 101]なことに注意する
+	bucket := make([]int, 102)
+	for _, p := range P {
+		bucket[p]++
+	}
+	bucket[X]++
+
+	// Xが一票なら、X自身が答え
+	if bucket[X] == 1 {
+		return X
+	}
+
+	// X未満かつ、未登場な値を検索
+	front := 0
+	for i, v := range bucket[:X] {
+		if v == 0 {
+			front = i
+		}
+	}
+
+	// Xより大きい かつ 未登場な値を検索
+	back := 0
+	for i, v := range bucket {
+		if i > X && v == 0 {
+			back = i
+			break
+		}
+	}
+
+	diff1 := lib.AbsInt(X - front)
+	diff2 := lib.AbsInt(X - back)
+
+	if diff1 < diff2 {
+		return front
+	} else if diff2 < diff1 {
+		return back
+	} else {
+		return lib.Min(front, back)
+	}
+
+}
+
+func TestAnswerABC170Cその1(t *testing.T) {
+	tests := []struct {
+		name string
+		X, N int
+		P    []int
+		want int
+	}{
+		{"入力例1", 6, 5, []int{4, 7, 10, 6, 5}, 8},
+		{"入力例2", 10, 5, []int{4, 7, 10, 6, 5}, 9},
+		{"入力例3", 100, 0, []int{}, 100},
+		{"答えが最大のケース", 100, 2, []int{99, 100}, 101},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := AnswerABC170Cその1(tt.X, tt.N, tt.P)
+
+			if got != tt.want {
+				t.Errorf("got %v want %v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ problemC/ABC170C_その2_test.go
@@ -0,0 +1,56 @@
+package problemC
+
+import (
+	"atgoder/lib"
+	"math"
+	"testing"
+)
+
+// [ABC170C - Forbidden List](https://atcoder.jp/contests/abc170/tasks/abc170_c)
+func AnswerABC170Cその2(X, N int, P []int) int {
+	// [0, 101]で全探索しつつ、Xとの距離をみて更新する
+	bucket := make([]int, 1000)
+	for _, p := range P {
+		bucket[p] = 1
+	}
+
+	var minDiff = math.MaxInt64
+	var ans int
+
+	for i := 0; i <= 101; i++ {
+		notUsed := bucket[i] == 0
+
+		diff := lib.AbsInt(i - X)
+
+		if notUsed && (diff < minDiff) {
+			minDiff = diff
+			ans = i
+		}
+	}
+
+	return ans
+}
+
+func TestAnswerABC170Cその2(t *testing.T) {
+	tests := []struct {
+		name string
+		X, N int
+		P    []int
+		want int
+	}{
+		{"入力例1", 6, 5, []int{4, 7, 10, 6, 5}, 8},
+		{"入力例2", 10, 5, []int{4, 7, 10, 6, 5}, 9},
+		{"入力例3", 100, 0, []int{}, 100},
+		{"答えが最大のケース", 100, 2, []int{99, 100}, 101},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := AnswerABC170Cその2(tt.X, tt.N, tt.P)
+
+			if got != tt.want {
+				t.Errorf("got %v want %v", got, tt.want)
+			}
+		})
+	}
+}
